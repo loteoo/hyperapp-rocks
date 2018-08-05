47af0c97ad2e91065b04169cc28b9e00696eeace
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-05 12:48:19 -0400

    loading

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -3,10 +3,17 @@ import {h} from 'hyperapp'
 import './projects.css'
 
 import {Project} from '../Project/Project.js'
+import {Spinner} from '../Spinner/Spinner.js'
+
 
 export const Projects = ({projects}) => (state, actions) => {
   if (!projects) {
     actions.loadProjects()
+    return (
+      <main>
+        <Spinner />
+      </main>
+    )
   } else {
     return (
       <main>
---
+++ src/views/Spinner/Spinner.js
@@ -0,0 +1,8 @@
+import {h} from 'hyperapp'
+
+import './spinner.css'
+
+export const Spinner = () => <div class="spinner" key="spinner"></div>
+
+// import {Spinner} from './Spinner/Spinner.js'
+// <Spinner />
