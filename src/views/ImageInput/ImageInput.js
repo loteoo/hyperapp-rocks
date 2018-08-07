822ae426a11162ec0b0efd728114fb650256cb94
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-07 00:59:30 -0400

    img input

---
+++ src/views/ImageInput/ImageInput.js
@@ -0,0 +1,16 @@
+import {h} from 'hyperapp'
+import cc from 'classcat'
+
+import './image-input.css'
+
+export const ImageInput = (props, children, {label = label || 'Label', name = name || 'name', setter} = props) => (
+  <div class={cc(['image-input', name])} key={name}>
+    <input type="file" name={name} id={name} oninput={ev => setter({[name]: ev.target.value})} {...props} setter={null} />
+    <div class="picker"></div>
+    <label for={name}>{label}</label>
+    <div class="border"></div>
+  </div>
+)
+
+// import {ImageInput} from './ImageInput/ImageInput.js'
+// <ImageInput label="First name" name="firstName" value={firstName} setter={set} />
--- src/views/ProjectForm/ProjectForm.js
+++ src/views/ProjectForm/ProjectForm.js
@@ -3,6 +3,7 @@ import {h} from 'hyperapp'
 import './project-form.css'
 
 import {NiceInput} from '../NiceInput/NiceInput.js'
+import {ImageInput} from '../ImageInput/ImageInput.js'
 
 
 export const ProjectForm = ({title, author, description, link, thumbnail, github}) => (state, actions) => (
@@ -15,7 +16,7 @@ export const ProjectForm = ({title, author, description, link, thumbnail, github
     <NiceInput type="textarea" label="Description" name="description" value={description} placeholder="Short project description..." setter={actions.setProjectForm} required />
     <NiceInput label="Author" name="author" value={author} placeholder="Who did this?" setter={actions.setProjectForm} />
     <NiceInput pattern="^https://github.com/(.*)" label="Github" name="github" value={github} placeholder="Sharing is caring :)" setter={actions.setProjectForm} />
-    {/* <ImageInput label="Image" name="thumbnail" required /> */}
+    <ImageInput label="Image" name="thumbnail" required />
 
 
     <div class="actions">
