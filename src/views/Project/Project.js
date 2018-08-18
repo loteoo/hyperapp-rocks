a630cc726771a683dc50f89eecc5208fce0f61e4
Alexandre Lotte <alexlotte16@gmail.com>
2018-08-18 15:16:35 -0400

    hovers

--- src/views/Project/Project.js
+++ src/views/Project/Project.js
@@ -2,11 +2,20 @@ import {h} from 'hyperapp'
 
 import {strapiUrl} from '../../utils/'
 
+import {Github} from '../Icons/Github.js'
+import {LinkIcon} from '../Icons/LinkIcon.js'
+
 import './project.css'
 
-export const Project = ({_id, title, link, description, thumbnail}) => (state, actions) => (
+export const Project = ({_id, title, author, github, link, description, thumbnail}) => (state, actions) => (
   <div class="project" key={_id}  onclick={ev => actions.location.go('/' + _id)}>
-    <div class="img" style={thumbnail ? {backgroundImage: `url(${strapiUrl(thumbnail.url)})`} : null}></div>
+    <div class="img">
+      <img src={strapiUrl(thumbnail.url)} alt={title} />
+      <div class="overlay">
+        {github ? <a href={github} onclick={ev => ev.stopPropagation()} target="_blank"><Github /></a> : null}
+        {link ? <a href={link} onclick={ev => ev.stopPropagation()} target="_blank"><LinkIcon /></a> : null}
+      </div>
+    </div>
     <div class="info">
       <h4>{title}</h4>
       <p class="description">{description}</p>
