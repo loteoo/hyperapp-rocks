ae800b50790484e546b912cdb188d82452b345ef
unknown <alexlotte16@gmail.com>
2018-08-27 16:16:54 -0400

    remove autoadjust txt area

--- src/views/common/NiceInput/NiceInput.js
+++ src/views/common/NiceInput/NiceInput.js
@@ -9,7 +9,7 @@ export const NiceInput = (props, children, {label = label || 'Label', name = nam
   <div class={cc(['nice-input', name])} key={name}>
     {
       type === 'textarea'
-      ? <textarea name={name} id={name} placeholder={placeholder} oninput={ev => {ev.target.style.height = (ev.target.scrollHeight + 2) + 'px'; setter({[name]: ev.target.value})}}  required={required}>{value}</textarea>
+      ? <textarea name={name} id={name} placeholder={placeholder} oninput={ev => setter({[name]: ev.target.value})} required={required}>{value}</textarea>
       : <input type={type} name={name} id={name} placeholder={placeholder} value={value} oninput={ev => setter({[name]: ev.target.value})} {...props} setter={null} />
     }
     <label for={name}>{label}</label>
