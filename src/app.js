5547a93c2bcfe27a86f54cd39f69485ed701e2ab
unknown <alexlotte16@gmail.com>
2018-08-27 13:31:37 -0400

    service worker with precache

--- src/app.js
+++ src/app.js
@@ -6,3 +6,19 @@ import {view} from './views/'
 
 window.main = app(state, actions, view, document.body)
 window.main.init()
+
+
+// Activate the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    let swPath = `service-worker.js`;
+    navigator.serviceWorker.register(swPath).then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
