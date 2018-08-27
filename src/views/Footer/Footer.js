0afe2186aa4455ed58f4390af9dbd6f925c1eee3
unknown <alexlotte16@gmail.com>
2018-08-26 22:28:54 -0400

    update footer & ghetto ssr

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -4,8 +4,8 @@ import './footer.css'
 
 export const Footer = () => (
   <footer>
-    <p>Made by Alexandre Lotte</p>
-    <p>Built with <a href="https://hyperapp.js.org/" target="_blank">Hyperapp</a> and  <a href="https://strapi.io/" target="_blank">Strapi</a>.</p>
+    <p>Made by Alexandre Lotte.</p>
+    <p>Built with <a href="https://hyperapp.js.org/" target="_blank">Hyperapp</a> and <a href="https://strapi.io/" target="_blank">Strapi</a>.</p>
     <p>All screenshots © of their respective owners.</p>
   </footer>
 )
