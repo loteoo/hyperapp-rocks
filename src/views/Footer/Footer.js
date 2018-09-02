58fee3e5dffab9b359848f8879088f8d9d0e958f
unknown <alexlotte16@gmail.com>
2018-09-02 12:08:48 -0400

    update footer

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -4,8 +4,8 @@ import './footer.css'
 
 export const Footer = () => (
   <footer>
-    <p>Made by Alexandre Lotte.</p>
-    <p>Built with <a href="https://hyperapp.js.org/" target="_blank">Hyperapp</a> and <a href="https://strapi.io/" target="_blank">Strapi</a>.</p>
+    <p>Made by <a href="https://github.com/loteoo" target="_blank">Alexandre Lotte</a>.</p>
+    <p>Built with <a href="https://hyperapp.js.org" target="_blank">Hyperapp</a>.</p>
     <p>All screenshots © of their respective owners.</p>
   </footer>
 )
