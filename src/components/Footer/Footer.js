b855e28d087b1095b763ad5c86b0bb0c3309b6e1
Alexandre Lotte <alexlotte16@gmail.com>
2018-09-24 15:30:41 -0400

    adjust footer links + nav arrow size

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './footer.css'
 
 export const Footer = () => (
   <footer>
-    <p>Made by <a href="https://github.com/loteoo" target="_blank">Alexandre Lotte</a>.</p>
-    <p>Built with <a href="https://hyperapp.js.org" target="_blank">Hyperapp</a>.</p>
+    <p>Made by <a href="https://alexlotte.ca/" target="_blank">Alexandre Lotte</a>.</p>
+    <p>Built with <a href="https://github.com/jorgebucaran/hyperapp" target="_blank">Hyperapp</a>.</p>
     <p>All screenshots © of their respective owners.</p>
   </footer>
 )
