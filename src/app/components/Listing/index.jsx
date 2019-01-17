ec085ed24e07ddb8c0681cbcd1c1cd07b23834e3
Alexandre Lotte <alexlotte16@gmail.com>
2019-01-16 21:36:34 -0500

    onmount

--- src/app/components/Listing/index.jsx
+++ src/app/components/Listing/index.jsx
@@ -14,7 +14,7 @@ import {HandleSearchForm} from '../Search/actions'
 
 // View
 export const Listing = ({state}) => (
-  <main class="listing" id="projects" role="main">
+  <main class="listing" id="projects" role="main" onmount={LoadProjects}>
     {
       state.listing
         ? <Results state={state} />
