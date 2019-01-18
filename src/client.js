ffb362ee0149b56c5ca9a3685282c10a103131db
Alexandre Lotte <alexlotte16@gmail.com>
2019-01-17 21:29:27 -0500

    adjust sw

--- src/client.js
+++ src/client.js
@@ -1,16 +1,33 @@
 // App dependencies
 import {app} from 'hyperapp'
-import {init} from './app/init'
+import {init as defaultState} from './app/init'
 import {view} from './app/view'
 
 import {SetPath} from './app/actions'
 import {enableOnMountDomEvent, Location} from './app/utils'
+
 enableOnMountDomEvent()
+
+
+
+// // Activate the service worker
+// if ('serviceWorker' in navigator) {
+//   const swPath = 'sw.js'
+//   navigator.serviceWorker.register(`/${swPath}`)
+//   .then((registration) => {
+//     console.log('Registration successful, scope is:', registration.scope)
+//   })
+//   .catch((error) => {
+//     console.log('Service worker registration failed, error:', error)
+//   });
+// }
+
+
 
 // Initialize the app on the document
 app({
   init: {
-    ...init,
+    ...defaultState,
     ...window.initialState,
     path: window.location.pathname
   },
@@ -23,17 +40,6 @@ app({
   },
   container: document
 })
-
-// // Activate the service worker
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('./service-worker.js')
-//   .then(function(registration) {
-//     console.log('Registration successful, scope is:', registration.scope);
-//   })
-//   .catch(function(error) {
-//     console.log('Service worker registration failed, error:', error);
-//   });
-// }
 
 
 console.log('%cSource code available here: \nhttps://github.com/loteoo/hyperapp-examples.', 'color: #a6a6a6; font-weight: 100; font-size: 1em;')
