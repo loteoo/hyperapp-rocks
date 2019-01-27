047a2ba42e53fc7808aa69bbbe90cb38e4f1d8ec
Alexandre Lotte <alexlotte16@gmail.com>
2019-01-27 09:09:34 -0500

    fix

--- src/client.js
+++ src/client.js
@@ -12,7 +12,7 @@ enableOnMountDomEvent()
 
 // Activate the service worker
 // if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('./sw.js')
+//   navigator.serviceWorker.register('/service-worker.js')
 //   .then((registration) => {
 //     console.log('Registration successful, scope is:', registration.scope)
 //   })
